86a9e9284fa7f04903838e52652b3a270df7c7eb
Soyalguien2324 <131987116+Soyalguien2324@users.noreply.github.com>
2023-10-17 16:24:55 +0200

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,20 +10,20 @@ sudo apt remove -y wine* &>/dev/null
 sudo apt remove -y *wine* &>/dev/null
 wget -nc https://dl.winehq.org/wine-builds/winehq.key &>/dev/null
 sudo apt install wine -y &>/dev/null
-echo "Parte 1/6.- Hecha!"
+echo "Parte 1/5.- Hecha!"
 echo "Espera por favor..."
 bash -c 'sudo apt-key add winehq.key' &>/dev/null
-echo "Parte 3/6 - Hecha!"
+echo "Parte 2/5 - Hecha!"
 bash -c 'echo "deb https://dl.winehq.org/wine-builds/debian/ bullseye main" | sudo tee /etc/apt/sources.list.d/wine_chrome_os_made_simple.list' &>/dev/null
 sudo dpkg --add-architecture i386
 sudo apt update -y &>/dev/null
 sudo apt install -y wine32 &>/dev/null
-echo "Parte 4/6 - Hecha!"
+echo "Parte 3/5 - Hecha!"
 echo "Espera por favor..."
 sudo apt update -y &>/dev/null
 sudo apt upgrade -y &>/dev/null
-echo "Parte 5/6 - Hecha!"
+echo "Parte 4/5 - Hecha!"
 winecfg
-echo "6/6 hecho!"
+echo "5/5 hecho!"
 echo "Ya tienes wine o windows"
 wine "$HOME/SteamSetup.exe"
